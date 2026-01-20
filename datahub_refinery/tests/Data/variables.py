b6f5441ec25132dea11508f0b690b8fee315cfa7
mpillai <mpillai@altimetrik.com>
2026-01-20 11:36:09 +0530

    Silver_ADF_Hash_Key_Validation

--- datahub_refinery/tests/Data/variables.py
+++ datahub_refinery/tests/Data/variables.py
@@ -1,7 +1,9 @@
 #*****************Environment Variable******************#
 landing_db="DEV_LANDING_ADF"
 bronze_db="DEV_BRONZE_ADF"
-
+silver_db="DEV_SILVER_ADF"
 #*****************Schema and Table name******************#
 airBnB_schema="AIRBNB"
-airBnB_table="AIRBNBDRIVERS"
+airBnB_drivers_table="AIRBNBDRIVERS"
+
+airBnB_listing_table = "AIRBNBLISTINGS"
